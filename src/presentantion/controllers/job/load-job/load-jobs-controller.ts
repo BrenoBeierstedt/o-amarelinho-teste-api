80c713d5f9a2a78b14a86b5e112ab2006c3876cd
Breno Beierstedt <breno.beierstedt@hotmail.com>
2022-10-03 19:10:43 -0300

    feat: ensure LoadJobsController returns 204 if is empty

--- src/presentantion/controllers/job/load-job/load-jobs-controller.ts
+++ src/presentantion/controllers/job/load-job/load-jobs-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, LoadJobs } from './load-jobs-controller-protocols'
-import { ok, serverError } from '../../../helpers/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http-helper'
 
 export class LoadJobsController implements Controller {
   constructor (private readonly loadJobs: LoadJobs) {}
@@ -7,7 +7,7 @@ export class LoadJobsController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const jobs = await this.loadJobs.load()
-      return ok(jobs)
+      return jobs.length ? ok(jobs) : noContent()
     } catch (e) {
       return serverError()
     }
